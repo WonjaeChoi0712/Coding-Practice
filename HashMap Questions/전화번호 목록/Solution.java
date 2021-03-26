9e2c67d464ca5be9e9394112071ec35d97941f50
WonjaeChoi0712 <81352026+WonjaeChoi0712@users.noreply.github.com>
2021-03-26 19:20:11 +0800

    Delete HashMap Questions directory

--- HashMap Questions/전화번호 목록/Solution.java
+++
@@ -1,6 +0,0 @@
-def solution(phone_book):
-    phone_book.sort()
-    for phone_num1, phone_num2 in zip(phone_book, phone_book[1:]):
-        if phone_num2.startswith(phone_num1):
-            return False
-    return True
